c7628259593198511e715d745194bcec48254311
Nidhin K <nidhinpattiam@gmail.com>
2024-11-10 00:34:55 +0530

    views setup

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const env = require('dotenv').config();
 const app =  express();
+const path = require('path');
+const userRouter = require('./routes/userRouter');
+const adminRouter = require('./routes/adminRouter');
 const db = require('./config/db');
-db()
+db();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/',userRouter);
+app.use('/admin',adminRouter)
 
 
 app.listen(process.env.PORT,()=>{
